bfae3ee1767ff5743f7a8fe987062484ddeef790
David Ker <snappy316@gmail.com>
2021-03-25 11:12:32 -0700

    Exercise 01 Extra Credit 4 Solution

--- src/exercise/01.js
+++ src/exercise/01.js
@@ -4,19 +4,20 @@
 import * as React from 'react'
 
 function countReducer(state, action) {
-  return {
-    ...state,
-    ...(typeof action === 'function' ? action(state) : action),
+  switch (action.type) {
+    case 'INCREMENT':
+      return {...state, count: state.count + action.step}
+    default:
+      return state
   }
 }
 
 function Counter({initialCount = 0, step = 1}) {
-  const [state, setState] = React.useReducer(countReducer, {
+  const [state, dispatch] = React.useReducer(countReducer, {
     count: initialCount,
   })
   const {count} = state
-  const increment = () =>
-    setState(currentState => ({count: currentState.count + step}))
+  const increment = () => dispatch({type: 'INCREMENT', step})
 
   return <button onClick={increment}>{count}</button>
 }
